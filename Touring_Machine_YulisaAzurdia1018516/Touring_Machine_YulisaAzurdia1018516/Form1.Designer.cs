aa7c82fa3f5f623719358317d1052ccfbefdd33b
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-05-05 01:03:31 -0600

    Turing machine for SUM added.

--- Touring_Machine_YulisaAzurdia1018516/Touring_Machine_YulisaAzurdia1018516/Form1.Designer.cs
+++ Touring_Machine_YulisaAzurdia1018516/Touring_Machine_YulisaAzurdia1018516/Form1.Designer.cs
@@ -28,33 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(167, 183);
+            this.button1.Location = new System.Drawing.Point(441, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Palíndromos",
+            "Patrones",
+            "Multiplicación",
+            "Suma",
+            "Resta"});
+            this.comboBox1.Location = new System.Drawing.Point(35, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(481, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(549, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 317);
+            this.ClientSize = new System.Drawing.Size(656, 317);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Touring_Machine_YulisaAzurdia1018516/Touring_Machine_YulisaAzurdia1018516/Form1.cs
+++ Touring_Machine_YulisaAzurdia1018516/Touring_Machine_YulisaAzurdia1018516/Form1.cs
@@ -12,9 +12,215 @@ namespace Touring_Machine_YulisaAzurdia1018516
 {
     public partial class Form1 : Form
     {
+        int actual_state = 0;
+        int head = 0;
+        int[] sumStates = new int[] { 0, 1, 2, 3 };
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.ColumnHeadersVisible = false;
+            label1.Text = "";
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Palíndromo")
+            {
+                MessageBox.Show("");
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Patrones")
+            {
+                MessageBox.Show("");
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Suma")
+            {
+                string text = textBox1.Text;
+                text.Trim();
+                var actualOne = text.ToArray();
+                string[] array = new string[(text.Length) * 2];
+                int i = 0;
+                array[0] = "B";
+                dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                this.dataGridView1.Rows[0].Cells[0].Value = array[0].ToString();
+                dataGridView1.Columns[0].Width = 20;
+                for (i = 0; i < actualOne.Length; i++)
+                {
+                    array[i+1] = actualOne[i].ToString();
+                    dataGridView1.Columns.Add((i+1).ToString(), (i+1).ToString());
+                    this.dataGridView1.Rows[0].Cells[i+1].Value = array[i+1];
+                    dataGridView1.Columns[i+1].Width = 20;
+
+                }
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    array[k] = "B";
+                    dataGridView1.Columns.Add(k.ToString(), "");
+                    this.dataGridView1.Rows[0].Cells[k].Value = array[i + 1];
+                    dataGridView1.Columns[k].Width = 20;
+                }
+                timer1.Enabled = true;
+                
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Multiplicación")
+            {
+                MessageBox.Show("");
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Resta")
+            {
+                MessageBox.Show("");
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string position = dataGridView1.Rows[0].Cells[head].Value.ToString();
+
+            switch (actual_state)
+            {
+                case (0):
+                    
+                    if (position == "|")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "X";                       
+                        head++;
+                        label1.Text = "q1";
+                        actual_state = sumStates[1];
+                        
+                    }
+                    else if (position == "+")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "+";
+                        head++;
+                        label1.Text = "q0";
+                        actual_state = sumStates[0];
+                    }
+                    else if (position == "=")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "=";                        
+                        head--;
+                        label1.Text = "q3";
+                        actual_state = sumStates[3];
+                        
+                        timer1.Enabled = false;
+                    }
+                    else if (position == "B")
+                    {                       
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Value = "B";
+                        head++;
+                        label1.Text = "q0";
+                        actual_state = sumStates[0];
+                    }
+                    break;
+                case (1):
+                    if (position == "|")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "|";
+                        head++;
+                        label1.Text = "q1";
+                        actual_state = sumStates[1];
+                    }
+                    else if (position == "+")
+                    {
+                        dataGridView1.Rows[0].Cells[head-1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "+";
+                        head++;
+                        label1.Text = "q1";
+                        actual_state = sumStates[1];
+                        
+                    }
+                    else if (position == "=")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "=";
+                        head++;
+                        label1.Text = "q1";
+                        actual_state = sumStates[1];
+                        
+                    }
+                    else if (position == "B")
+                    {
+                        dataGridView1.Rows[0].Cells[head - 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "|";
+                        head--;
+                        label1.Text = "q2";
+                        actual_state = sumStates[2];                        
+                    }
+                    break;
+                case (2):
+                    if (position == "|")
+                    {
+                        dataGridView1.Rows[0].Cells[head + 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "|";
+                        head--;
+                        label1.Text = "q2";
+                        actual_state = sumStates[2];                       
+                    }
+                    else if (position == "+")
+                    {
+                        dataGridView1.Rows[0].Cells[head + 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "+";
+                        head--;
+                        label1.Text = "q2";
+                        actual_state = sumStates[2];
+                        
+                    }
+                    else if (position == "=")
+                    {
+                        dataGridView1.Rows[0].Cells[head + 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "=";
+                        head--;
+                        label1.Text = "q2";
+                        actual_state = sumStates[2];
+                    }
+                    else if (position == "X")
+                    {
+                        dataGridView1.Rows[0].Cells[head + 1].Style.BackColor = Color.White;
+                        dataGridView1.Rows[0].Cells[head].Style.BackColor = Color.Aqua;
+                        dataGridView1.Rows[0].Cells[head].Value = "|";
+                        head++;
+                        label1.Text = "q0";
+                        actual_state = sumStates[0];
+                        
+                    }
+                    break;
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Refresh();
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
